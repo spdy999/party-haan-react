7a019bef8f63a0ab81e3b4274b431c0a87cc63a9
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-08 20:49:45 +0700

    tested router

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import './App.css';
 import Typography from '@material-ui/core/Typography';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import axios from 'axios';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+
 export interface IParty {
   capacity: number;
   id: number;
@@ -21,24 +25,27 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <Typography variant="h1" component="h2" gutterBottom>
-          h1. Heading
-          <AccessAlarmIcon />
-        </Typography>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        {/* <nav>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/user/:id">User</Link>
+          </div>
+        </nav> */}
+        <Switch>
+          {/* <Route path="/user/:id">
+            <User />
+          </Route> */}
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Login.tsx
@@ -0,0 +1,4 @@
+const Login = () => {
+  return <div>This is the login page</div>;
+};
+export default Login;
