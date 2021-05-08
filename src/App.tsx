a8c02bb778d43be0032fb697cb5c95b48942f846
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-08 19:10:20 +0700

    initial setup axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Typography from '@material-ui/core/Typography';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import axios from 'axios';
+export interface IParty {
+  capacity: number;
+  id: number;
+  imgUrl: string;
+  name: string;
+}
 
 function App() {
+  const getParties = async (): Promise<void> => {
+    const { data }: { data: IParty[] } = await axios.get('/parties');
+    console.log(data);
+  };
+  useEffect(() => {
+    void getParties();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/roboto';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:4000';
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    // Edit request config
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    // Edit response config
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
 
 ReactDOM.render(
   <React.StrictMode>
