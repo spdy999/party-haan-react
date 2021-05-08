55f6101f19bf415da112dedd989e5495d89d536e
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-08 18:34:34 +0700

    test font and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Typography variant="h1" component="h2" gutterBottom>
+          h1. Heading
+          <AccessAlarmIcon />
+        </Typography>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
