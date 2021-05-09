e13ae38619d3c54f741d244e24e7360d4b3a8505
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-09 17:51:26 +0700

    add AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import { AppWrapper } from './context/state';
@@ -9,14 +10,54 @@ import { LoginContextWrapper } from './context/login/LoginProvider';
 import Register from './components/Register';
 import { RegisterContextWrapper } from './context/register/RegisterProvider';
 import CreateParty from './components/party/CreateParty';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <AppWrapper>
         <RegisterContextWrapper>
           <LoginContextWrapper>
             <PartyContextWrapper>
+              <AppBar position="static">
+                <Toolbar>
+                  <IconButton
+                    edge="start"
+                    className={classes.menuButton}
+                    color="inherit"
+                    aria-label="menu"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" className={classes.title}>
+                    News
+                  </Typography>
+                  <Button color="inherit">Login</Button>
+                </Toolbar>
+              </AppBar>
               <BrowserRouter>
                 <Switch>
                   <Route
