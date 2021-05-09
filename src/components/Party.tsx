e3a71585442bd773f99d644f88adbeffca715211
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-09 17:18:33 +0700

    add container, add create btn

--- src/components/Party.tsx
+++ src/components/Party.tsx
@@ -2,15 +2,27 @@ import { usePartyContext } from '../context/party/PartyProvider';
 import PartyCard from './party/PartyCard';
 import { IParty } from '../context/party/state';
 import Typography from '@material-ui/core/Typography';
+import { Box, Container, IconButton } from '@material-ui/core';
+import { Add } from '@material-ui/icons';
 export default function Party() {
   const { state } = usePartyContext();
 
   return (
-    <div>
-      <Typography variant="h6">ปาร์ตี้ทั้งหมด</Typography>
+    <Container>
+      <Box
+        display="flex"
+        flexDirection="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography variant="h6">ปาร์ตี้ทั้งหมด</Typography>
+        <IconButton>
+          <Add />
+        </IconButton>
+      </Box>
       {state.parties.map((party: IParty) => (
         <PartyCard key={party.id} party={party} />
       ))}
-    </div>
+    </Container>
   );
 }
